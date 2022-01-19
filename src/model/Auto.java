fed603de56e6e789d38480c35653b940142d4ddc
TimKrauth13 <timkrauth7@gmail.com>
2022-01-19 19:16:17 +0100

    Klasse Auto hinzugefügt und vererbt

---
+++ src/model/Auto.java
@@ -0,0 +1,38 @@
+package model;
+
+public abstract class Auto {
+
+	private float xAuto;
+	private float yAuto;
+	
+	
+	public Auto(float xAuto, float yAuto) {
+		this.xAuto = xAuto;
+		this.yAuto = yAuto;
+	}
+
+
+	public float getxAuto() {
+		return xAuto;
+	}
+
+
+	public void setxAuto(float xAuto) {
+		this.xAuto = xAuto;
+	}
+
+
+	public float getyAuto() {
+		return yAuto;
+	}
+
+
+	public void setyAuto(float yAuto) {
+		this.yAuto = yAuto;
+	}
+	
+	
+	
+}
+
+
